40166983abf2ee01deabd3bbd758f1e8411df860
gusujin <gusu@likelion.net>
2023-11-19 20:58:33 +0900

    fix: convert html tag

--- src/utils/convertHTMLtag.ts
+++ src/utils/convertHTMLtag.ts
@@ -1,6 +1,6 @@
 const convertHTMLtag = (str: string) => {
   if (str.includes("&#039;")) return (str = str.replace(/&(#039);/g, "'"));
-  if (str.includes("&quot;")) return (str = str.replace(/&quot;/g, '"'));
+  if (str.includes("&quot;")) return (str = str.replace(/&quot;/g, '\"'));
   return str;
 };
 
